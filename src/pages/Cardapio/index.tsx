4f526752ebcff521e32a9e50a34202f3dbbd52d4
CaNeoN28 <carlosfelipe.st28@gmail.com>
2022-03-29 16:12:48 -0300

    First Commit

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,9 @@
+import styles from "Cardapio.module.scss";
+
+export default function Cardapio(){
+    return(
+        <nav>
+            <button className="botao">Botao</button>
+        </nav>
+    )
+}
