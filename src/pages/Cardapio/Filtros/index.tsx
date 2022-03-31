eb618b9f83369489d8d79c1fc85b26309028d7c0
CaNeoN28 <carlosfelipe.st28@gmail.com>
2022-03-31 15:16:08 -0300

    Selecionando filtros e ClassNames

--- src/pages/Cardapio/Filtros/index.tsx
+++ src/pages/Cardapio/Filtros/index.tsx
@@ -1,15 +1,36 @@
 import styles from "./Filtros.module.scss";
 import filtros from "./filtros.json";
 import { type } from "@testing-library/user-event/dist/type";
+import classNames from "classnames";
 
 type IFiltro = typeof filtros[0];
 
-export default function Filtros(){
-    function selecionarFiltros(opcao: IFiltro){}
+interface Props{
+    filtro: number | null,
+    setFiltro: React.Dispatch<React.SetStateAction<number | null>>
+}
+
+export default function Filtros({filtro, setFiltro} : Props){
+    function selecionarFiltros(opcao: IFiltro){
+        if(opcao.id == filtro)
+            return setFiltro(null)
+
+        return setFiltro(opcao.id)
+    }
+
     return(
         <div className={styles.filtros}>
             {filtros.map((opcao) => (
-                <button className = {styles.filtros__filtro}key={opcao.id} onClick={() => selecionarFiltros(opcao)}>
+                <button 
+                className = {
+                    classNames({
+                        [styles.filtros__filtro]: true,
+                        [styles["filtros__filtro--ativo"]]: filtro==opcao.id
+                    })
+                }
+                key={opcao.id} 
+                onClick={() => selecionarFiltros(opcao)}
+                >
                     {opcao.label}
                 </button>
             ))}
