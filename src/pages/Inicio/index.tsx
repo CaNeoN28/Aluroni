55c766680590048568c63e44baa6e8409d78cba9
CaNeoN28 <carlosfelipe.st28@gmail.com>
2022-04-04 13:55:50 -0300

    Adicionando pratos ao inicio

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -1,14 +1,27 @@
 import itens from 'data/cardapio.json';
+import styles from './Inicio.module.scss';
 
-export default function Inicio(){
-    return(
-        <section>
-            <h3>
-                Recomendações do Chef
-            </h3>
-            <div>
+export default function Inicio() {
+	let itensSelecionados = [...itens];
+	itensSelecionados = itensSelecionados.sort(() => 0.5 - Math.random()).splice(0,3);
 
-            </div>
-        </section>
-    );
+	return (
+		<section>
+			<h3 className={styles.titulo}>
+				Recomendações do Chef
+			</h3>
+			<div className={styles.recomendados}>
+				{itensSelecionados.map(item => (
+					<div key={item.id} className={styles.recinebdado}>
+						<div className={styles.recomendado__imagem}>
+							<img src={item.photo} alt={item.title} />
+						</div>
+						<button className={styles.recomendado__botao}>
+							Saber mais
+						</button>
+					</div>
+				))}
+			</div>
+		</section>
+	);
 }
